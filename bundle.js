1e8e0caa5a7251114b8d30d023abf166c3bdd8ff
HarrisMBrad <bmichaelh13@gmail.com>
2021-02-08 21:59:51 -0500

    removing libs, making room for options

--- bundle.js
+++
@@ -1,8 +0,0 @@
-//How to define this..? I need to research lodash... Not sure yet. - BH
-
-var _ = require('lodash');
-
-console.log(_);
-
-var array = [1, 2, 3, 4];
-console.log('answer: ', _.without(array, 1));
